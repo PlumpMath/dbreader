0fd97448d6f537c0b445fb00da4e2aca27b4adbc
Esperanza <hola@mi-mina.com>
2016-04-09 22:00:15 +0200

    wip

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.ashafa/clutch "0.4.0"]
+                 [cheshire "5.5.0"]])
--- src/dbreader/core.clj
+++ src/dbreader/core.clj
@@ -1,6 +1,29 @@
-(ns dbreader.core)
+(ns dbreader.core
+  (:require [com.ashafa.clutch :as c]
+            [cheshire.core :as ch] ))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def test1 (c/get-database "test1"))
+
+(def gt (c/get-database "grossestreffen"))
+
+(def doc1 (c/get-document gt "e99927417d4509d627861e163db1fd3a"))
+
+
+
+(c/put-document test1 (dissoc doc1 :_attachments))
+
+
+
+
+
+
+(defn create-application-from-profile [profile-id year]
+  (let [init-dok (c/put-document test1 {})
+        doc-id (:_id init-dok)
+        _ (c/copy-document test1 profile-id init-dok)
+        new-dok (c/get-document test1 doc-id)
+        _ (c/put-document test1 (assoc (select-keys new-dok [:_id :_rev :content])
+                                     :type "application" :status "draft" :user (:owner new-dok) :created (str "09/04/2016") :year year))]
+    doc-id))
+
+(create-application-from-profile "e99927417d4509d627861e163db1fd3a" 2016)
